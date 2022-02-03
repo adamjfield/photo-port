1cd352a55710bd4426bda03f76d9fcf42bf227b4
Adam Field <adamjfield@gmail.com>
2022-02-02 23:15:02 -0600

    fix errors

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
+
 import { validateEmail } from '../../utils/helpers';
 
-function ContactForm(params) {
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
   const [errorMessage, setErrorMessage] = useState('');
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
   const { name, email, message } = formState;
 
-  function handleChange(e) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
     if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      // isValid conditional statement
       if (!isValid) {
         setErrorMessage('Your email is invalid.');
       } else {
@@ -27,53 +31,30 @@ function ContactForm(params) {
         setErrorMessage('');
       }
     }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-  }
+  };
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
-  //JSX
   return (
     <section>
-      <h1>Contact me</h1>
-      <form id='contact-form' onSubmit={handleSubmit}>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='name'>Name:</label>
-          <input
-            type='text'
-            name='name'
-            defaultValue={name}
-            onBlur={handleChange}
-          />
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor='email'>Email address:</label>
-          <input
-            type='email'
-            name='email'
-            defaultValue={email}
-            onBlur={handleChange}
-          />
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor='message'>Message:</label>
-          <textarea
-            name='message'
-            rows='5'
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-              </div>
-              {errorMessage && (
-                  <div>
-                      <p className='error-text'>{errorMessage}</p>
-                  </div>
-              )}
-        <button type='submit'>Submit</button>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
       </form>
     </section>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,40 +1,26 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
+    currentCategory,
     setContactSelected,
   } = props;
 
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
-
   return (
-    <header className='flex-row px-1'>
+    <header className="flex-row px-1">
       <h2>
-        <a data-testid='link' href='/'>
-          <span role='img' aria-label='camera'>
-            {' '}
-            📸
-          </span>{' '}
-          Oh Snap!
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
-        <ul className='flex-row'>
-          <li className='mx-2'>
-            <a
-              data-testid='about'
-              href='#about'
-              onClick={() => setContactSelected(false)}
-            >
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
@@ -44,10 +30,8 @@ function Nav(props) {
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name &&
-                !contactSelected &&
-                `navActive`
-              }`}
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
               key={category.name}
             >
               <span
